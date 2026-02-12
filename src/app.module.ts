88757c45e7daa99837e53d459fd1d3c2f370fd80
palash2397 <palashpatil55@gmail.com>
2026-02-12 10:59:53 +0530

    Remove client module from app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,14 +8,14 @@ import { TaskModule } from './modules/task/task.module';
 import { AwsModule } from './modules/aws/aws.module';
 import { EventModule } from './modules/event/event.module';
 import { ContactModule } from './modules/contact/contact.module';
-import { ClientModule } from './client/client.module';
+
 
 
 @Module({
   imports: [DatabaseModule, ConfigModule.forRoot({
     isGlobal: true,
     envFilePath: '.env',
-  }), UserModule, CallLogModule, CaseModule, TaskModule, AwsModule, EventModule, ContactModule, ClientModule],
+  }), UserModule, CallLogModule, CaseModule, TaskModule, AwsModule, EventModule, ContactModule],
   controllers: [],
   providers: [],
 })
