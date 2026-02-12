c25c460dc2b671d32b34f4e1ccd410e806c9df3f
palash2397 <palashpatil55@gmail.com>
2026-02-12 14:57:39 +0530

    Update message service with proper model injections

--- src/modules/message/message.service.ts
+++ src/modules/message/message.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { Model, Types } from 'mongoose';
+import { ApiResponse } from '../../utils/helper/ApiResponse';
+import { Msg } from '../../utils/helper/responseMsg';
+
+
+import { Message, MessageDocument } from './schemas/message.schema';
+import { User, UserDocument } from '../user/schemas/user.schema';
+import { Case, CaseDocument } from '../case/schemas/case.schema';   
 
 @Injectable()
-export class MessageService {}
+export class MessageService {
+    constructor(
+        @InjectModel('Message') private messageModel: Model<MessageDocument>,
+        @InjectModel('User') private userModel: Model<UserDocument>,
+        @InjectModel('Case') private caseModel: Model<CaseDocument>,
+    ) {}
+}
