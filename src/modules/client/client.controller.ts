bfcecbfc48ca83137c6891dc5426920f8a093de8
palash2397 <palashpatil55@gmail.com>
2026-02-12 12:11:18 +0530

    Add client controller and update DTO

--- src/modules/client/client.controller.ts
+++ src/modules/client/client.controller.ts
@@ -1,7 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Req,
+  Param,
+  Patch,
+  Get,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
+
+import { FileInterceptor } from '@nestjs/platform-express';
+import type { Request } from 'express';
+import { JwtAuthGuard } from '../auth/jwt/jwt-auth.guard';
+
 import { ClientService } from './client.service';
 
 @Controller('client')
 export class ClientController {
   constructor(private readonly clientService: ClientService) {}
+
+  @Post('/create')
+  @UseGuards(JwtAuthGuard)
+  create(@Body() createClientDto: any) {
+    return this.clientService.create(createClientDto);
+  }
+
 }
