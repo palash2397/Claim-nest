3d68463962648015620133b79dbc9c40f8f57502
palash2397 <palashpatil55@gmail.com>
2026-02-12 18:59:56 +0530

    Add call log controller with create and all endpoints

--- src/modules/call-log/call-log.controller.ts
+++ src/modules/call-log/call-log.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Req, Get } from '@nestjs/common';
 import { CallLogService } from './call-log.service';
+import { JwtAuthGuard } from '../auth/jwt/jwt-auth.guard';
+import { CreateCallLogDto } from './dto/create-call-log.dto';
 
 @Controller('call-log')
 export class CallLogController {
   constructor(private readonly callLogService: CallLogService) {}
+
+  @Post("/create")
+  @UseGuards(JwtAuthGuard)
+  create(@Body() dto: CreateCallLogDto, @Req() req: any) {
+    return this.callLogService.createCallLog(dto, req.user.id);
+  }
+
+  @Get("/all")
+  @UseGuards(JwtAuthGuard)
+  findAll() {
+    return this.callLogService.allCallLog();
+  }
 }
