156965c645791e974b4eb20c1913ff763d3371c6
palash2397 <palashpatil55@gmail.com>
2026-02-12 11:30:46 +0530

    Create client module with proper structure

--- src/modules/client/client.module.ts
+++ src/modules/client/client.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { ClientService } from './client.service';
 import { ClientController } from './client.controller';
 
+import { ClientSchema } from './schemas/client.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'Client', schema: ClientSchema }])],
   controllers: [ClientController],
   providers: [ClientService],
+  exports: [ClientService, MongooseModule.forFeature([{ name: 'Client', schema: ClientSchema }])],
 })
 export class ClientModule {}
