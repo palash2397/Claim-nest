59d07564152d7a28ff5b06cd0a8acf4dd2d7f886
palash2397 <palashpatil55@gmail.com>
2026-02-12 18:03:42 +0530

    Update call log service and module structure

--- src/modules/call-log/call-log.module.ts
+++ src/modules/call-log/call-log.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { CallLogService } from './call-log.service';
 import { CallLogController } from './call-log.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { CallLog, CallLogSchema } from './schemas/callLog.schema';
+import { Case, CaseSchema } from '../case/schemas/case.schema';
+import { User, UserSchema } from '../user/schemas/user.schema';
 
 @Module({
   controllers: [CallLogController],
   providers: [CallLogService],
-  exports: [CallLogService],
+  exports: [
+    CallLogService,
+    MongooseModule.forFeature([{ name: CallLog.name, schema: CallLogSchema }]),
+  ],
+  imports: [
+    MongooseModule.forFeature([
+      { name: CallLog.name, schema: CallLogSchema },
+      { name: Case.name, schema: CaseSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
 })
 export class CallLogModule {}
