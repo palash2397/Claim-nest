b6246f0c48219885142976282f7dc1254b2007fd
palash2397 <palashpatil55@gmail.com>
2026-02-13 14:06:22 +0530

    Complete activity log controller with create and get endpoints

--- src/modules/activity-log/activity-log.controller.ts
+++ src/modules/activity-log/activity-log.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Param } from '@nestjs/common';
 import { ActivityLogService } from './activity-log.service';
 
-@Controller('activity-log')
+import { JwtAuthGuard } from '../auth/jwt/jwt-auth.guard';
+import { UseGuards } from '@nestjs/common';
+
+import { CreateActivityLogDto } from './dto/create-activity-log.dto';
+
+@Controller('case/activity-log')
 export class ActivityLogController {
   constructor(private readonly activityLogService: ActivityLogService) {}
+
+  @Post('/')
+  @UseGuards(JwtAuthGuard)
+  create(@Body() dto: CreateActivityLogDto, @Req() req: any) {
+    return this.activityLogService.create(dto, req.user.id);
+  }
+
+
+  @Get("/:id")
+  @UseGuards(JwtAuthGuard)
+  getActivity(@Req() req: any, @Param('id') id: string) {
+    return this.activityLogService.getByCase(id);
+  }
+
 }
