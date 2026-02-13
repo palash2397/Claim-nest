19f0da5cd1017ec2c807bab28a25267777f401f2
palash2397 <palashpatil55@gmail.com>
2026-02-13 11:07:13 +0530

    Add delete functionality to call log controller

--- src/modules/call-log/call-log.controller.ts
+++ src/modules/call-log/call-log.controller.ts
@@ -1,7 +1,11 @@
-import { Controller, Post, Body, UseGuards, Req, Get } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Req, Get, Patch, Delete } from '@nestjs/common';
 import { CallLogService } from './call-log.service';
 import { JwtAuthGuard } from '../auth/jwt/jwt-auth.guard';
+
+
 import { CreateCallLogDto } from './dto/create-call-log.dto';
+import { UpdateCallLogDto } from './dto/update-call-log.dto';
+
 
 @Controller('call-log')
 export class CallLogController {
@@ -20,6 +24,18 @@ export class CallLogController {
     return this.callLogService.allCallLog();
   }
 
+
+  @Patch("/update")
+  @UseGuards(JwtAuthGuard)
+  update(@Body() dto: UpdateCallLogDto) {
+    return this.callLogService.updateCallLog(dto);
+  }
+   
+  // @Delete("/delete/:id")
+  // @UseGuards(JwtAuthGuard)
+  // delete(@Req() req: any) {
+  //   return this.callLogService.deleteCallLog(req.params.id);
+  // }
 
   @Get("/by-id/:id")
   @UseGuards(JwtAuthGuard)
