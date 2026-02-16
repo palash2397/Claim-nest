199cb988df4e97b4c559c08371d57a1a55a62c0c
palash2397 <palashpatil55@gmail.com>
2026-02-16 14:13:52 +0530

    Update excel module with Mongoose integration

--- src/modules/excel/excel.module.ts
+++ src/modules/excel/excel.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { ExcelService } from './excel.service';
 import { ExcelController } from './excel.controller';
 
+import { MongooseModule } from '@nestjs/mongoose';
+import { Excel, ExcelSchema } from './schemas/excel.schema';
+
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Excel.name, schema: ExcelSchema }])],
   controllers: [ExcelController],
   providers: [ExcelService],
-  exports: [ExcelService],
+  exports: [ExcelService, MongooseModule.forFeature([{ name: Excel.name, schema: ExcelSchema }])],
 })
 export class ExcelModule {}
