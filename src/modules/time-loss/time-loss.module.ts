9a347ee50bc35b61abbe96a2056a12df4c21600d
palash2397 <palashpatil55@gmail.com>
2026-02-16 10:38:55 +0530

    Update time-loss module with Case and User schema imports

--- src/modules/time-loss/time-loss.module.ts
+++ src/modules/time-loss/time-loss.module.ts
@@ -2,8 +2,24 @@ import { Module } from '@nestjs/common';
 import { TimeLossService } from './time-loss.service';
 import { TimeLossController } from './time-loss.controller';
 
+import { MongooseModule } from '@nestjs/mongoose';
+import { TimeLoss, TimeLossSchema } from './schemas/time-loss.schema';
+import { Case, CaseSchema } from '../case/schemas/case.schema';
+import { User, UserSchema } from '../user/schemas/user.schema';
+
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: TimeLoss.name, schema: TimeLossSchema },
+      { name: Case.name, schema: CaseSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
   controllers: [TimeLossController],
   providers: [TimeLossService],
+
+  exports: [TimeLossService, MongooseModule.forFeature([
+    { name: TimeLoss.name, schema: TimeLossSchema }
+  ])],
 })
 export class TimeLossModule {}
