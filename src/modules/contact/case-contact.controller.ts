b1c821f1551f140e0201ee6d04ff0bdd6713139f
palash2397 <palashpatil55@gmail.com>
2026-02-18 11:40:04 +0530

    Add JWT authentication to case-contact controller

--- src/modules/contact/case-contact.controller.ts
+++ src/modules/contact/case-contact.controller.ts
@@ -9,11 +9,22 @@ import {
   Delete,
   Get,
   Query,
+  
 } from '@nestjs/common';
+
 import { CaseContactService } from './case-contact.service';
+import { CreateCaseContactDto } from './dto/create-case-contact.dto';
+import { JwtAuthGuard } from '../auth/jwt/jwt-auth.guard';
+
 
 @Controller('case-contact')
 export class CaseContactController {
   constructor(private readonly caseContactService: CaseContactService) {}
   
+
+  @Post("/create")
+  @UseGuards(JwtAuthGuard)
+  create(@Body() dto: CreateCaseContactDto) {
+    return this.caseContactService.create(dto);
+  }
 }
