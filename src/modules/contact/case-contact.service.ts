eb139781e4c9d949cabd6df80f74cc1a6be9330e
palash2397 <palashpatil55@gmail.com>
2026-02-18 11:05:30 +0530

    Complete case-contact service with model injections

--- src/modules/contact/case-contact.service.ts
+++ src/modules/contact/case-contact.service.ts
@@ -1,8 +1,21 @@
+
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { Model, Types } from 'mongoose';
+import { ApiResponse } from '../../utils/helper/ApiResponse';
+import { Msg } from '../../utils/helper/responseMsg';
 
 
+import { CaseContact, CaseContactDocument } from './schemas/case-contact.schema';
+import { Contact, ContactDocument } from './schemas/contact.schema';
+import { Case, CaseDocument } from '../case/schemas/case.schema';
 
 @Injectable()
 export class CaseContactService {
-    
+    constructor(
+        @InjectModel(CaseContact.name) private caseContactModel: Model<CaseContactDocument>,
+        @InjectModel(Contact.name) private contactModel: Model<ContactDocument>,
+        @InjectModel(Case.name) private caseModel: Model<CaseDocument>,
+    ) {}
 }
