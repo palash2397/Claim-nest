a8eefdccdfc4840b894a86bba5ec5b5dae8c7fa5
palash2397 <palashpatil55@gmail.com>
2026-02-18 11:35:02 +0530

    Complete case-contact service create method with validation and error handling

--- src/modules/contact/case-contact.service.ts
+++ src/modules/contact/case-contact.service.ts
@@ -1,4 +1,3 @@
-
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose';
@@ -6,28 +5,61 @@ import { Model, Types } from 'mongoose';
 import { ApiResponse } from '../../utils/helper/ApiResponse';
 import { Msg } from '../../utils/helper/responseMsg';
 
-
-import { CaseContact, CaseContactDocument } from './schemas/case-contact.schema';
+import {
+  CaseContact,
+  CaseContactDocument,
+} from './schemas/case-contact.schema';
 import { Contact, ContactDocument } from './schemas/contact.schema';
 import { Case, CaseDocument } from '../case/schemas/case.schema';
-
 
 import { CreateCaseContactDto } from './dto/create-case-contact.dto';
 
 @Injectable()
 export class CaseContactService {
-    constructor(
-        @InjectModel(CaseContact.name) private caseContactModel: Model<CaseContactDocument>,
-        @InjectModel(Contact.name) private contactModel: Model<ContactDocument>,
-        @InjectModel(Case.name) private caseModel: Model<CaseDocument>,
-    ) {}
+  constructor(
+    @InjectModel(CaseContact.name)
+    private caseContactModel: Model<CaseContactDocument>,
+    @InjectModel(Contact.name) private contactModel: Model<ContactDocument>,
+    @InjectModel(Case.name) private caseModel: Model<CaseDocument>,
+  ) {}
 
-
-    async create(dto: CreateCaseContactDto) {
-       const caseDoc = await this.caseModel.findById(dto.caseId);
-       if (!caseDoc) {
+  async create(dto: CreateCaseContactDto) {
+    try {
+      const caseDoc = await this.caseModel.findById(dto.caseId);
+      if (!caseDoc) {
         return new ApiResponse(404, {}, Msg.CASE_NOT_FOUND);
-       }
-        
+      }
+
+      const contactDoc = await this.contactModel.findById(dto.contactId);
+      if (!contactDoc) {
+        return new ApiResponse(404, {}, Msg.CONTACT_NOT_FOUND);
+      }
+
+      //    // Check if contact is already associated with this case
+      //    const existingCaseContact = await this.caseContactModel.findOne({
+      //     caseId: caseDoc._id,
+      //     contactId: contactDoc._id
+      //    });
+
+      //    if (existingCaseContact) {
+      //     return new ApiResponse(400, {}, Msg.CASE_CONTACT_ALREADY_EXISTS);
+      //    }
+
+      // Create new case contact
+      const caseContact = new this.caseContactModel({
+        caseId: new Types.ObjectId(dto.caseId),
+        contactId: new Types.ObjectId(dto.contactId),
+        roleOnCase: dto.roleOnCase,
+        isPrimary: dto.isPrimary || false,
+        caseNotes: dto.caseNotes,
+      });
+
+      await caseContact.save();
+
+      return new ApiResponse(201, caseContact, Msg.CONTACT_CREATED);
+    } catch (error) {
+      console.error('Error creating case contact:', error);
+      return new ApiResponse(500, {}, Msg.SERVER_ERROR);
     }
+  }
 }
