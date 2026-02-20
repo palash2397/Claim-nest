62763d87d1cfb75245367f0d30271ef9965a185b
palash2397 <palashpatil55@gmail.com>
2026-02-20 15:44:06 +0530

    Add update endpoint to fee controller

--- src/modules/fee/fee.controller.ts
+++ src/modules/fee/fee.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Post, Body, UseGuards, Req } from '@nestjs/common';
 import { FeeService } from './fee.service';
-import { CreateFeeDto } from './dto/create-fee.dto';
 import { JwtAuthGuard } from '../auth/jwt/jwt-auth.guard';
 import { RoleGuard } from '../auth/roles/roles.guard';
 
@@ -8,6 +7,8 @@ import { RoleGuard } from '../auth/roles/roles.guard';
 import { Roles } from '../auth/roles/roles.decorator';
 import { UserRole } from '../user/schemas/user.schema';
 
+import { CreateFeeDto } from './dto/create-fee.dto';
+import { UpdateFeeDto } from './dto/update-fee.dto';
 
 @UseGuards(JwtAuthGuard, RoleGuard)
 
@@ -19,5 +20,10 @@ export class FeeController {
   @Post("/create")
   create(@Body() dto: CreateFeeDto, @Req() req: any) {
     return this.feeService.create(dto, req.user.id);
+  }
+
+  @Post("/update")
+  update(@Body() dto: UpdateFeeDto) {
+    return this.feeService.update(dto);
   }
 }
