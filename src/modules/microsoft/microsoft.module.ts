23b1673c032413ade873adbc0cecd923210c7c5b
palash2397 <palashpatil55@gmail.com>
2026-02-27 15:26:31 +0530

    Add Calendar service to Microsoft module
    - Generate Calendar service for Microsoft Graph API integration
    - Add Calendar service spec file for testing
    - Update Microsoft module to include CalendarService
    - Prepare for Microsoft calendar operations and events management

--- src/modules/microsoft/microsoft.module.ts
+++ src/modules/microsoft/microsoft.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { MicrosoftService } from './microsoft.service';
 import { MicrosoftController } from './microsoft.controller';
 import { OutlookService } from './outlook/outlook.service';
+import { CalenderService } from './calender/calender.service';
 ;
 
 @Module({
   controllers: [MicrosoftController],
-  providers: [MicrosoftService, OutlookService],
+  providers: [MicrosoftService, OutlookService, CalenderService],
   exports:[MicrosoftService]
 })
 export class MicrosoftModule {}
