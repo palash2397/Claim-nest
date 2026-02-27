219a6fb386eb8075454808db39d550c04510eae4
palash2397 <palashpatil55@gmail.com>
2026-02-27 15:24:36 +0530

    Add Microsoft Outlook service integration
    - Create OutlookService for Microsoft Graph API integration
    - Add Microsoft module configuration for OAuth and Graph services
    - Prepare infrastructure for Microsoft email and calendar operations
    - Set up service for future Microsoft 365 integration

--- src/modules/microsoft/microsoft.module.ts
+++ src/modules/microsoft/microsoft.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MicrosoftService } from './microsoft.service';
 import { MicrosoftController } from './microsoft.controller';
+import { OutlookService } from './outlook.service';
 
 @Module({
   controllers: [MicrosoftController],
-  providers: [MicrosoftService],
+  providers: [MicrosoftService, OutlookService, OutlookService],
 })
 export class MicrosoftModule {}
--- src/modules/microsoft/microsoft.service.ts
+++ src/modules/microsoft/microsoft.service.ts
@@ -2,3 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class MicrosoftService {}
+
+
+// nest g s service-name
+
+
+// nest g s modules/microsoft/outlook.service.ts
