ca32243686eb867258ed84a68d4214aa01014014
palash2397 <palashpatil55@gmail.com>
2026-02-27 15:25:24 +0530

    Add Outlook service to Microsoft module
    - Add OutlookService to Microsoft module providers and exports
    - Generate Outlook service spec file for testing
    - Update Microsoft module to include both MicrosoftService and OutlookService
    - Prepare for Microsoft Graph API integration with Outlook services

--- src/modules/microsoft/microsoft.module.ts
+++ src/modules/microsoft/microsoft.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MicrosoftService } from './microsoft.service';
 import { MicrosoftController } from './microsoft.controller';
-import { OutlookService } from './outlook.service';
+import { OutlookService } from './outlook/outlook.service';
+;
 
 @Module({
   controllers: [MicrosoftController],
-  providers: [MicrosoftService, OutlookService, OutlookService],
+  providers: [MicrosoftService, OutlookService],
+  exports:[MicrosoftService]
 })
 export class MicrosoftModule {}
