86fc694dcf7bfd6fd956c496aee7196fea25a5ef
palash2397 <palashpatil55@gmail.com>
2026-02-27 15:35:33 +0530

    Add OneDrive service and clean up Microsoft module
    - Add OnedriveService to Microsoft module providers and exports
    - Generate OneDrive service for Microsoft Graph API integration
    - Remove unused spec files for Outlook and Calendar services
    - Update module exports to include all Microsoft services
    - Prepare for OneDrive file operations and storage management

--- src/modules/microsoft/microsoft.module.ts
+++ src/modules/microsoft/microsoft.module.ts
@@ -3,11 +3,16 @@ import { MicrosoftService } from './microsoft.service';
 import { MicrosoftController } from './microsoft.controller';
 import { OutlookService } from './outlook/outlook.service';
 import { CalenderService } from './calender/calender.service';
-;
+import { OnedriveService } from './onedrive/onedrive.service';
 
 @Module({
   controllers: [MicrosoftController],
-  providers: [MicrosoftService, OutlookService, CalenderService],
-  exports:[MicrosoftService]
+  providers: [
+    MicrosoftService,
+    OutlookService,
+    CalenderService,
+    OnedriveService,
+  ],
+  exports: [MicrosoftService, OutlookService, CalenderService, OnedriveService],
 })
 export class MicrosoftModule {}
