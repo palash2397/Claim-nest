b2382e9eff0179e20bb1c3c8d13cdb9d54ed750c
palash2397 <palashpatil55@gmail.com>
2026-03-06 16:51:26 +0530

    Update AWS service

--- src/modules/aws/aws.service.ts
+++ src/modules/aws/aws.service.ts
@@ -5,6 +5,36 @@ import * as mime from 'mime-types';
 
 // aws setup
 @Injectable()
+// export class AwsService {
+//   private s3 = new S3({
+//     region: process.env.AWS_REGION, // credentials automatically picked from EC2 role
+//   });
+
+//   async uploadFile(key: string, buffer: Buffer, mimeType: string) {
+//     return this.s3
+//       .upload({
+//         Bucket: process.env.AWS_S3_BUCKET!,
+//         Key: key,
+//         Body: buffer,
+//         ContentType: mimeType,
+//       })
+//       .promise();
+//   }
+
+//   async getSignedFileUrl(key: string, expiresIn = 3600) {
+//     const contentType = mime.lookup(key) || 'application/octet-stream';
+
+//     return this.s3.getSignedUrlPromise('getObject', {
+//       Bucket: process.env.AWS_S3_BUCKET!,
+//       Key: key,
+//       Expires: expiresIn,
+//       ResponseContentDisposition: 'inline',
+//       ResponseContentType: contentType,
+//     });
+//   }
+// }
+
+
 export class AwsService {
   private s3 = new S3({
     region: process.env.AWS_REGION,
