d0d6aa8f1c7c8c945a6041b6f5495a002f53775a
palash2397 <palashpatil55@gmail.com>
2026-03-07 18:58:16 +0530

    Add ContactRole, ContactType, and ContactStatus enums to contact enum file

--- src/common/enums/contact.enum.ts
+++ src/common/enums/contact.enum.ts
@@ -4,3 +4,20 @@ export enum ContactRole {
   EMPLOYER = 'Employer',
   OTHER = 'Other',
 }
+
+
+export enum ContactType {
+  CLIENT = 'Client',
+  ATTORNEY = 'Attorney',
+  EMPLOYER = 'Employer',
+  MEDICAL = 'Medical',
+  OTHER = 'Other',
+}
+
+export enum ContactStatus {
+  POTENTIAL = 'Potential',
+  INQUIRY = 'Inquiry',
+  CLIENT = 'Client',
+  CALLBACK = 'Callback',
+  CLOSED = 'Closed',
+}
