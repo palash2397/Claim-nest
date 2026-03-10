6b1e6eead35d6bb82c0ab65f0af731f1bec15b76
palash2397 <palashpatil55@gmail.com>
2026-03-10 17:17:28 +0530

    Add getEmails method to OutlookService for fetching user emails

--- src/modules/microsoft/outlook/outlook.service.ts
+++ src/modules/microsoft/outlook/outlook.service.ts
@@ -4,4 +4,12 @@ import { GraphService } from '../services/graph.service';
 @Injectable()
 export class OutlookService {
   constructor(private readonly graphService: GraphService) {}
+
+  async getEmails(userId: string) {
+    return this.graphService.graphRequest(
+      userId,
+      'GET',
+      '/me/messages?$top=25&$select=subject,from,receivedDateTime,bodyPreview',
+    );
+  }
 }
