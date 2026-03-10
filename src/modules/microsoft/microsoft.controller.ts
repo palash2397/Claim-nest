067967560303c9a52233bba54f05f9553e146d43
palash2397 <palashpatil55@gmail.com>
2026-03-10 17:21:09 +0530

    Add JWT guard and OutlookService injection to Microsoft controller

--- src/modules/microsoft/microsoft.controller.ts
+++ src/modules/microsoft/microsoft.controller.ts
@@ -12,6 +12,10 @@ import { OutlookService } from './outlook/outlook.service';
 import { MicrosoftService } from './microsoft.service';
 
 @Controller('microsoft')
+@UseGuards(AuthGuard('jwt'))
 export class MicrosoftController {
-  constructor(private readonly microsoftService: MicrosoftService) {}
+  constructor(
+    private readonly microsoftService: MicrosoftService,
+    private readonly outlookService: OutlookService,
+  ) {}
 }
