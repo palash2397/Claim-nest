56dc4e04232ee63dda17929cfb961197c513f411
palash2397 <palashpatil55@gmail.com>
2026-03-11 17:56:24 +0530

    Add getEvents method to CalenderService for Microsoft Graph API calls

--- src/modules/microsoft/calender/calender.service.ts
+++ src/modules/microsoft/calender/calender.service.ts
@@ -1,7 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { GraphService } from '../services/graph.service';
 
+
 @Injectable()
 export class CalenderService {
   constructor(private readonly graphService: GraphService) {}
+
+  async getEvents(userId: string) {
+    return this.graphService.graphRequest(
+      userId,
+      'GET',
+      '/me/events?$select=subject,organizer,start,end,location',
+    );
+  }
 }
