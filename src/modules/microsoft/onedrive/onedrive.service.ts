0e368bc1f2b8b99f176adcaa72a7953db7604853
palash2397 <palashpatil55@gmail.com>
2026-03-11 18:27:07 +0530

    Add listFiles method to OnedriveService for Microsoft Graph API

--- src/modules/microsoft/onedrive/onedrive.service.ts
+++ src/modules/microsoft/onedrive/onedrive.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { GraphService } from '../services/graph.service';
 
-
 @Injectable()
 export class OnedriveService {
   constructor(private readonly graphService: GraphService) {}
+
+  async listFiles(userId: string) {
+    return this.graphService.graphRequest(
+      userId,
+      'GET',
+      '/me/drive/root/children',
+    );
+  }
 }
