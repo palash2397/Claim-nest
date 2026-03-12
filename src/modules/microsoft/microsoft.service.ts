7150a3f4e6d3fc505027d9e0e67935c1424b1388
palash2397 <palashpatil55@gmail.com>
2026-03-12 11:47:23 +0530

    Add User model injection to Microsoft service constructor

--- src/modules/microsoft/microsoft.service.ts
+++ src/modules/microsoft/microsoft.service.ts
@@ -2,8 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
+import { User, UserDocument } from '../user/schemas/user.schema';
+
 @Injectable()
-export class MicrosoftService {}
+export class MicrosoftService {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<UserDocument>,
+  ) {}
+}
 
 
 // nest g s service-name
