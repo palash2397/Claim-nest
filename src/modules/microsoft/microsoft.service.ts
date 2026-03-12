ecacfff6567f19305a7024512bf21edb64c9024d
palash2397 <palashpatil55@gmail.com>
2026-03-12 12:10:20 +0530

    Add error handling to microsoftStatus method in Microsoft service

--- src/modules/microsoft/microsoft.service.ts
+++ src/modules/microsoft/microsoft.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { ApiResponse } from 'src/utils/helper/ApiResponse';
+import { Msg } from 'src/utils/helper/responseMsg';
 
 import { User, UserDocument } from '../user/schemas/user.schema';
 
@@ -12,23 +14,28 @@ export class MicrosoftService {
   ) {}
 
   async microsoftStatus(userId: string) {
-    const user = await this.userModel
-      .findById(userId)
-      .select('email microsoftAccessToken microsoftRefreshToken');
+    try {
+      const user = await this.userModel
+        .findById(userId)
+        .select('email microsoftAccessToken microsoftRefreshToken');
 
-    if (!user) {
+      if (!user) {
+        return {
+          connected: false,
+        };
+      }
+
+      const connected =
+        !!user.microsoftAccessToken && !!user.microsoftRefreshToken;
+
       return {
-        connected: false,
+        connected,
+        email: connected ? user.email : null,
       };
+    } catch (error) {
+      console.log(`error while checking microsoft status`, error);
+      return new ApiResponse(500, {}, Msg.SERVER_ERROR);
     }
-
-    const connected =
-      !!user.microsoftAccessToken && !!user.microsoftRefreshToken;
-
-    return {
-      connected,
-      email: connected ? user.email : null,
-    };
   }
 }
 
