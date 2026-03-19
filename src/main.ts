7d3d2717c86bd821917e5497526a718e7dea7fe0
palash2397 <palashpatil55@gmail.com>
2026-03-19 14:21:22 +0530

    Remove hardcoded IP address from app.listen

--- src/main.ts
+++ src/main.ts
@@ -88,7 +88,7 @@ async function bootstrap() {
     },
   });
 
-  await app.listen(process.env.PORT ?? 4004, '127.0.0.1');
+  await app.listen(process.env.PORT ?? 4004);
   console.log(`🚀 Server is running on port ${process.env.PORT}`);
 }
 bootstrap();
