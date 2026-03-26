06c2ba18cc91e4cb506d23943e2c7241e3a4f829
palash2397 <palashpatil55@gmail.com>
2026-03-26 10:38:09 +0530

    Update OneDrive service with console logging

--- src/modules/microsoft/onedrive/onedrive.service.ts
+++ src/modules/microsoft/onedrive/onedrive.service.ts
@@ -2,22 +2,29 @@ import { Injectable } from '@nestjs/common';
 import { GraphService } from '../services/graph.service';
 
 import axios from 'axios';
+import { ApiResponse } from 'src/utils/helper/ApiResponse';
+import { Msg } from 'src/utils/helper/responseMsg';
 
 @Injectable()
 export class OnedriveService {
   constructor(private readonly graphService: GraphService) {}
 
   async listFiles(userId: string) {
-    const result = await this.graphService.graphRequest(
-      userId,
-      'GET',
-      '/me/drive/root/children?$select=id,name,size,lastModifiedDateTime,file,folder,webUrl',
-    );
+    try {
+      const result = await this.graphService.graphRequest(
+        userId,
+        'GET',
+        '/me/drive/root/children?$select=id,name,size,lastModifiedDateTime,file,folder,webUrl',
+      );
 
-    console.log(result);
-    return {
-      files: result.value,
-    };
+      console.log(result);
+      return {
+        files: result.value,
+      };
+    } catch (error) {
+      console.log(`error who called listFiles: ${error}`);
+      return new ApiResponse(500, {}, Msg.SERVER_ERROR);
+    }
   }
 
   async uploadFile(userId: string, fileName: string, fileBuffer: Buffer) {
