8d19c78dc5d943d24edb5e1cf087ebc9097e6aa7
palash2397 <palashpatil55@gmail.com>
2026-04-04 10:30:39 +0530

    Simplify UpdateUserDto by removing _id field and validation imports

--- src/modules/user/dto/update-user.dto.ts
+++ src/modules/user/dto/update-user.dto.ts
@@ -1,10 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsMongoId, IsNotEmpty } from 'class-validator';
+
 
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
-  @IsMongoId()
-  @IsNotEmpty()
-  _id: string;
+
 }
