feeed2a36adefeff439d621e4d696c1a8f3eee0a
palash2397 <palashpatil55@gmail.com>
2026-04-17 16:38:05 +0530

    Add additional schema imports to case module

--- src/modules/case/case.module.ts
+++ src/modules/case/case.module.ts
@@ -7,6 +7,7 @@ import { CaseCounter, CaseCounterSchema } from './schemas/case-counter.schema';
 import { ActivityLog, ActivityLogSchema } from '../activity-log/schemas/activity-log.schema';
 import { Note, NoteSchema } from '../notes/schemas/create-note.schema';
 import { CallLog, CallLogSchema } from '../call-log/schemas/callLog.schema';
+import { CaseEmail, CaseEmailSchema } from '../case-mail/schemas/case-mail.schema';
 
 import { UserModule } from '../user/user.module';
 import { AwsModule } from '../aws/aws.module';
@@ -16,6 +17,10 @@ import { AwsModule } from '../aws/aws.module';
     MongooseModule.forFeature([
       { name: Case.name, schema: CaseSchema },
       { name: CaseCounter.name, schema: CaseCounterSchema },
+      { name: CaseEmail.name, schema: CaseEmailSchema },
+      { name: ActivityLog.name, schema: ActivityLogSchema },
+      { name: Note.name, schema: NoteSchema },
+      { name: CallLog.name, schema: CallLogSchema },
     ]),
     UserModule,
     AwsModule,
