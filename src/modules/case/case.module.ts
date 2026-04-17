5aaa3bd22b57c4eac2fe7354bcb8ddb60b9d7cff
palash2397 <palashpatil55@gmail.com>
2026-04-17 15:42:25 +0530

    Add ActivityLog schema import and format case module

--- src/modules/case/case.module.ts
+++ src/modules/case/case.module.ts
@@ -4,14 +4,28 @@ import { CaseController } from './case.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Case, CaseSchema } from './schemas/case.schema';
 import { CaseCounter, CaseCounterSchema } from './schemas/case-counter.schema';
+import { ActivityLog, ActivityLogSchema } from '../activity-log/schemas/activity-log.schema';
 
 import { UserModule } from '../user/user.module';
 import { AwsModule } from '../aws/aws.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Case.name, schema: CaseSchema }, { name: CaseCounter.name, schema: CaseCounterSchema }]), UserModule, AwsModule],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Case.name, schema: CaseSchema },
+      { name: CaseCounter.name, schema: CaseCounterSchema },
+    ]),
+    UserModule,
+    AwsModule,
+  ],
   controllers: [CaseController],
   providers: [CaseService],
-  exports: [CaseService, MongooseModule.forFeature([{ name: Case.name, schema: CaseSchema }, { name: CaseCounter.name, schema: CaseCounterSchema }])],
+  exports: [
+    CaseService,
+    MongooseModule.forFeature([
+      { name: Case.name, schema: CaseSchema },
+      { name: CaseCounter.name, schema: CaseCounterSchema },
+    ]),
+  ],
 })
 export class CaseModule {}
