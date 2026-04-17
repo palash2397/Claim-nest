a9849d133bad81f31c34b052d739fbbd5abf712b
palash2397 <palashpatil55@gmail.com>
2026-04-17 16:59:04 +0530

    Add CaseMessage schema import and fix exports array

--- src/modules/case/case.module.ts
+++ src/modules/case/case.module.ts
@@ -8,6 +8,7 @@ import { ActivityLog, ActivityLogSchema } from '../activity-log/schemas/activity
 import { Note, NoteSchema } from '../notes/schemas/create-note.schema';
 import { CallLog, CallLogSchema } from '../call-log/schemas/callLog.schema';
 import { CaseEmail, CaseEmailSchema } from '../case-mail/schemas/case-mail.schema';
+import { CaseMessage, CaseMessageSchema } from '../case-message/schemas/case-msg.schema';
 
 import { UserModule } from '../user/user.module';
 import { AwsModule } from '../aws/aws.module';
@@ -21,6 +22,7 @@ import { AwsModule } from '../aws/aws.module';
       { name: ActivityLog.name, schema: ActivityLogSchema },
       { name: Note.name, schema: NoteSchema },
       { name: CallLog.name, schema: CallLogSchema },
+      { name: CaseMessage.name, schema: CaseMessageSchema },
     ]),
     UserModule,
     AwsModule,
@@ -31,7 +33,7 @@ import { AwsModule } from '../aws/aws.module';
     CaseService,
     MongooseModule.forFeature([
       { name: Case.name, schema: CaseSchema },
-      { name: CaseCounter.name, schema: CaseCounterSchema },
+      { name: CaseCounter.name, schema: CaseCounterSchema }
     ]),
   ],
 })
