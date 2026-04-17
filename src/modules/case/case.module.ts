8da7d955ef21ea5314188dc646e27d95c38ff21c
palash2397 <palashpatil55@gmail.com>
2026-04-17 17:18:56 +0530

    Add additional schema imports and format case module

--- src/modules/case/case.module.ts
+++ src/modules/case/case.module.ts
@@ -4,11 +4,28 @@ import { CaseController } from './case.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Case, CaseSchema } from './schemas/case.schema';
 import { CaseCounter, CaseCounterSchema } from './schemas/case-counter.schema';
-import { ActivityLog, ActivityLogSchema } from '../activity-log/schemas/activity-log.schema';
+import {
+  ActivityLog,
+  ActivityLogSchema,
+} from '../activity-log/schemas/activity-log.schema';
 import { Note, NoteSchema } from '../notes/schemas/create-note.schema';
 import { CallLog, CallLogSchema } from '../call-log/schemas/callLog.schema';
-import { CaseEmail, CaseEmailSchema } from '../case-mail/schemas/case-mail.schema';
-import { CaseMessage, CaseMessageSchema } from '../case-message/schemas/case-msg.schema';
+import {
+  CaseEmail,
+  CaseEmailSchema,
+} from '../case-mail/schemas/case-mail.schema';
+import {
+  CaseMessage,
+  CaseMessageSchema,
+} from '../case-message/schemas/case-msg.schema';
+import {
+  TimeLoss,
+  TimeLossSchema,
+} from '../case-time-loss/schemas/case-time-loss.schema';
+import {
+  DocumentFile,
+  DocumentFileSchema,
+} from '../case-documents/schemas/create-document.schema';
 
 import { UserModule } from '../user/user.module';
 import { AwsModule } from '../aws/aws.module';
@@ -23,6 +40,8 @@ import { AwsModule } from '../aws/aws.module';
       { name: Note.name, schema: NoteSchema },
       { name: CallLog.name, schema: CallLogSchema },
       { name: CaseMessage.name, schema: CaseMessageSchema },
+      { name: DocumentFile.name, schema: DocumentFileSchema },
+      { name: TimeLoss.name, schema: TimeLossSchema },
     ]),
     UserModule,
     AwsModule,
@@ -33,7 +52,7 @@ import { AwsModule } from '../aws/aws.module';
     CaseService,
     MongooseModule.forFeature([
       { name: Case.name, schema: CaseSchema },
-      { name: CaseCounter.name, schema: CaseCounterSchema }
+      { name: CaseCounter.name, schema: CaseCounterSchema },
     ]),
   ],
 })
